f9bd8a3be3deb6ab6f81c6056d4afb90a03f34f5
Rohitdubey67 <31805232+Rohitdubey67@users.noreply.github.com>
2020-04-30 22:32:02 +0530

    Create Day-13-Contiguous Array.cpp

---
+++ Day-13-Contiguous Array.cpp
@@ -0,0 +1,33 @@
+/*
+Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+*/
+class Solution {
+public:
+    int findMaxLength(vector<int>& a) {
+        int i,k=0,p=0;
+        unordered_map<int,int>m;
+        m[0]=-1;
+        for(i=0;i<a.size();i++)
+        {
+            if(a[i]==0)
+                a[i]=-1;
+            k+=a[i];
+            if(m.find(k)!=m.end())
+            {
+              p=max(p,i-m[k]); 
+            }
+            else
+            m[k]=i;
+        }
+        return p;
+    }
+};
